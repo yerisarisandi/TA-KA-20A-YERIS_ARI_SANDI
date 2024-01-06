68a399f70e8b25e3e4a430c0777229e0043dd609
yerisarisandi <yeri_ari_sandi@teknokrat.ac.id>
2024-01-06 23:25:42 +0700

    menambah function ambil data

--- Program/app/Http/Controllers/Login.php
+++ Program/app/Http/Controllers/Login.php
@@ -23,5 +23,59 @@ class Login extends Controller
         return view("login");
     }
 
+    // buat fungsi untuk ambil data 
+    function getLogin(Request $req)
+    {
+        // collecting data input (username dan password)
+        // "username" = nama variabel array
+        // "$req->username" = nama object yang dikirim dari "fetch"
+        $data = [
+
+            "username" => $req->username,
+            "password" => base64_encode(md5($req->password)),
+            "ingat" => $req->ingat,
+        ];
+
+        // cek data/record dari "tb_user"
+        // jika data ditemukan
+        if(count($this->model->getData($data["username"],$data["password"])) == 1)
+        {
+            // buat session
+            // $req->session()->put("username_loginapp",$data["username"]);
+
+            //ambil data username
+            $query = $this->model->getData($data["username"],$data["password"]);
+            $nama = $query->last()->nama;
+            $req->session()->put("username_loginapp",$nama);
+
+            //jika ingat = 1
+            if($data["ingat"] == 1)
+            {
+            //buat cookie
+            Cookie::queue("cookie_loginapp",$nama,120);
+            }
+
+            $output = 1;
+        }
+        // jika data tidak ditemukan
+        else
+        {
+            $output = 0;
+        }
+        // kirim nilai variabel "$output" ke "then(result)" pada fetch
+        echo json_encode(["output" => $output]);
+    }
+
+    //fungsi untuk logout
+    function setLogout(Request $req){
+        //hapus session
+        $req->session()->forget('username_loginapp');
+
+        //hapus cookie
+        Cookie::queue(Cookie::forget("cookie_loginapp"));
+
+        //alihkan ke halaman login
+        return redirect("/login");
+    }
     
 }
