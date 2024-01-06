94610b82da379413e11fcdd9020d40b1ecbc4fd7
yerisarisandi <yeri_ari_sandi@teknokrat.ac.id>
2024-01-06 23:26:34 +0700

    menambah function get data pada model

--- Program/app/Models/Mlogin.php
+++ Program/app/Models/Mlogin.php
@@ -11,4 +11,16 @@ class Mlogin extends Model
     // use HasFactory;
     // buat fungsi untuk ambil ambil data login
     
+    function getdata($username, $password)
+    {
+        // cek data username dan password
+        $query = DB::table('tb_user')
+        ->select("username","password","nama")
+        ->where("username",$username)
+        // ->orwhere("1=1")
+        ->where("password",$password)
+        // ->whereRaw("username = '$username'")
+        ->get();
+        return $query;
+    }
 }
